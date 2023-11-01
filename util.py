95ba2d4e8841ccec633b0503813bdd9335c226f9
Lucar Capristano Carrillo <consultor_dgpmacdf23@mef.gob.pe>
2023-11-01 17:01:57 -0500

    fix

--- util.py
+++ util.py
@@ -1,16 +1,20 @@
 import datetime
 
+
 def getyear():
     today = datetime.date.today()
     return today.year
+
 
 def getmonth():
     today = datetime.date.today()
     return today.month
 
+
 def getday():
     today = datetime.date.today()
     return today.day
+
 
 print(getyear())
 print(getmonth())
